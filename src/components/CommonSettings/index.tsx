94eb49ef981cc5bf2b07d43c000da7d476d2b586
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-10-13 14:07:40 +0300

    fill common settings inputs

--- src/components/CommonSettings/index.tsx
+++ src/components/CommonSettings/index.tsx
@@ -4,7 +4,8 @@ import { useAppSelector } from "@/hooks/useAppSelector";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { emailRegEx } from "@/utils/consts";
 import EditAvatar from "../EditAvatar";
-import { useState } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
+import { RequestStatus } from "@/models/RequestStatus";
 
 type Inputs = {
   email: string;
@@ -24,7 +25,48 @@ function CommonSettings() {
 
   const [newAvatar, setNewAvatar] = useState("");
 
+  // made controlled inputs with react-hook-form because when
+  // set value through react-hook-form - input's label doesn't
+  // lift up, it stays behind input's text
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [fullName, setFullName] = useState("");
+
   const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  useEffect(() => {
+    if (user.status !== RequestStatus.IDLE) {
+      return;
+    }
+
+    setEmail(user.email);
+    setUsername(user.username);
+    setFullName(user.fullName);
+  }, [user]);
+
+  function onEmailChange(e: ChangeEvent<HTMLInputElement>) {
+    const { onChange } = register("email");
+    setEmail(e.target.value);
+    onChange(e);
+  }
+
+  function onUsernameChange(e: ChangeEvent<HTMLInputElement>) {
+    const { onChange } = register("username");
+    setUsername(e.target.value);
+    onChange(e);
+  }
+
+  function onFullnameChange(e: ChangeEvent<HTMLInputElement>) {
+    const { onChange } = register("fullName");
+    setFullName(e.target.value);
+    onChange(e);
+  }
+
+  function onResetClick() {
+    setEmail("");
+    setUsername("");
+    setFullName("");
+  }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -34,9 +76,11 @@ function CommonSettings() {
       />
       <Styled.InputsWrapper>
         <TextField
+          value={email}
+          {...register("email", { required: true, pattern: emailRegEx })}
+          onChange={onEmailChange}
           label="Email"
           variant="outlined"
-          {...register("email", { required: true, pattern: emailRegEx })}
           helperText={
             errors.email
               ? errors.email.type === "required"
@@ -47,22 +91,26 @@ function CommonSettings() {
           error={Boolean(errors.email)}
         />
         <TextField
+          value={username}
+          {...register("username", { required: true })}
+          onChange={onUsernameChange}
           label="Username"
           variant="outlined"
-          {...register("username", { required: true })}
           error={Boolean(errors.username)}
           helperText={errors.username ? "Required" : " "}
         />
         <TextField
+          value={fullName}
+          {...register("fullName", { required: true })}
+          onChange={onFullnameChange}
           label="Fullname"
           variant="outlined"
-          {...register("fullName", { required: true })}
           error={Boolean(errors.fullName)}
           helperText={errors.fullName ? "Required" : " "}
         />
       </Styled.InputsWrapper>
       <Styled.FormButtons>
-        <Button variant="outlined" type="reset">
+        <Button variant="outlined" type="reset" onClick={onResetClick}>
           Reset
         </Button>
         <Button variant="contained" type="submit">
