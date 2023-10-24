2fa3b40776ff000dae4d4eaf21716e8f44c2b976
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-10-24 13:36:07 +0300

    add go back functionality

--- src/components/ChatTopBar/index.tsx
+++ src/components/ChatTopBar/index.tsx
@@ -1,16 +1,19 @@
 import { IconButton } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { useRouter } from "next/router";
 
 import UserSummary from "../UserSummary";
 
 import * as Styled from "./ChatTopBar.styled";
 
 function ChatTopBar() {
+  const router = useRouter();
+
   return (
     <Styled.TopBar>
       <Styled.TopBarLeft>
-        <IconButton>
+        <IconButton onClick={() => router.push("/chats")}>
           <ArrowBackIcon />
         </IconButton>
         <UserSummary
