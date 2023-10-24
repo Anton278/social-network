96512a60716da20c53e82f24f16b107219a2fbf8
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-10-24 18:48:23 +0300

    add show error

--- src/pages/chats/index.tsx
+++ src/pages/chats/index.tsx
@@ -17,7 +17,10 @@ import * as Styled from "@/styles/Chats.styled";
 
 function Chats() {
   const dispatch = useAppDispatch();
+
   const [showFriends, setShowFriends] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
   const userStatus = useAppSelector((state) => state.user.status);
   const userId = useAppSelector(selectUserId);
   const chatIds = useAppSelector((state) => state.user.chats);
@@ -25,7 +28,6 @@ function Chats() {
   const [userChats, setUserChats] = useState<Chat[]>([]);
   const chats = useAppSelector((state) => state.chats.chats);
   const chatsStatus = useAppSelector((state) => state.chats.status);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     dispatch(getChats());
@@ -43,6 +45,55 @@ function Chats() {
     setIsLoading(false);
   }, [userStatus, chatsStatus]);
 
+  if (userStatus === RequestStatus.Error) {
+    return (
+      <Layout>
+        <Styled.TopBar>
+          <Button
+            variant="outlined"
+            startIcon={<AddIcon />}
+            onClick={() => setShowFriends(true)}
+          >
+            New chat
+          </Button>
+        </Styled.TopBar>
+        <Typography color={"error"}>Failed to load user</Typography>
+      </Layout>
+    );
+  }
+  if (chatsStatus === RequestStatus.Error) {
+    return (
+      <Layout>
+        <Styled.TopBar>
+          <Button
+            variant="outlined"
+            startIcon={<AddIcon />}
+            onClick={() => setShowFriends(true)}
+          >
+            New chat
+          </Button>
+        </Styled.TopBar>
+        <Typography color={"error"}>Failed to load chats</Typography>
+      </Layout>
+    );
+  }
+  if (isLoading) {
+    return (
+      <Layout>
+        <Styled.TopBar>
+          <Button
+            variant="outlined"
+            startIcon={<AddIcon />}
+            onClick={() => setShowFriends(true)}
+          >
+            New chat
+          </Button>
+        </Styled.TopBar>
+        <Typography>Loading...</Typography>
+      </Layout>
+    );
+  }
+
   return (
     <>
       <Layout>
@@ -55,9 +106,7 @@ function Chats() {
             New chat
           </Button>
         </Styled.TopBar>
-        {isLoading ? (
-          <Typography>Loading...</Typography>
-        ) : (
+        {userChats.length ? (
           <Styled.ChatsList>
             {userChats.map((chat) => {
               const interlocutor = chat.participants.find(
@@ -70,6 +119,8 @@ function Chats() {
               );
             })}
           </Styled.ChatsList>
+        ) : (
+          <Typography>Here will be displayed chats</Typography>
         )}
       </Layout>
       <FriendsDialog
