e5fce99e26bfc6ff004468081317391f53dab936
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-11-01 19:37:25 +0200

    take firebase cfg from env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,13 +20,13 @@ import "react-toastify/dist/ReactToastify.css";
 import "@/styles/globals.css";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyChS_7eX32-sx4De1n8PK9UiShJS-qT4Mk",
-  authDomain: "social-network-a5d55.firebaseapp.com",
-  projectId: "social-network-a5d55",
-  storageBucket: "social-network-a5d55.appspot.com",
-  messagingSenderId: "173062262693",
-  appId: "1:173062262693:web:0c91bbc0eb342d8423224c",
-  measurementId: "G-NDTV4X0SMY",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
