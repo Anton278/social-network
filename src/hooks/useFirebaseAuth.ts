cc93f08328ef5b9a78ee88643de85d26f9d27c60
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-11-01 21:15:03 +0200

    add build fix

--- src/hooks/useFirebaseAuth.ts
+++
@@ -1,8 +0,0 @@
-import { FirebaseContext } from "@/contexts/firebase";
-import { useContext } from "react";
-
-export function useFirebaseAuth() {
-  const { auth, isAuthStateLoading } = useContext(FirebaseContext);
-
-  return { auth, isLoading: isAuthStateLoading };
-}
